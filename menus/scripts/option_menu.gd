22d64aa03bcfb20e180139c959dcafd2cbdad84f
Zeta80 <axxel1700@gmail.com>
2025-02-05 00:13:46 +0100

    func for volumes

--- menus/scripts/option_menu.gd
+++ menus/scripts/option_menu.gd
@@ -3,7 +3,9 @@ extends Control
 @onready var master_slide: HSlider = $AudioControl/Buttons_cont/Master_slide
 func _ready() -> void:
 	print(db_to_linear(AudioServer.get_bus_volume_db(0)))
+	master_slide.value_changed.connect(_on_master_slide_drag_ended)
 	master_slide.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -15,8 +17,7 @@ func _on_button_back_pressed() -> void:
 
 func _on_master_slide_drag_ended(value_changed: bool) -> void:
 	if value_changed:
-		AudioServer.set_bus_volume_db(0, linear_to_db(master_slide.value))
-		print("funzione", db_to_linear(AudioServer.get_bus_volume_db(0)))
+		change_value(master_slide.value, 0)
 
 
 func _on_music_slide_drag_ended(value_changed: bool) -> void:
@@ -25,3 +26,9 @@ func _on_music_slide_drag_ended(value_changed: bool) -> void:
 
 func _on_sfx_slide_drag_ended(value_changed: bool) -> void:
 	pass # Replace with function body.
+	
+	
+func change_value(value: float, index: int):
+	AudioServer.set_bus_volume_db(index, linear_to_db(value))
+	print("funzione", db_to_linear(AudioServer.get_bus_volume_db(0)))
+	return value
