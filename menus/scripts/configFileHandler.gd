6edc4409b18741cf507540452d16ff568a30320a
Zeta80 <axxel1700@gmail.com>
2025-02-05 02:47:03 +0100

    comments

--- menus/scripts/configFileHandler.gd
+++ menus/scripts/configFileHandler.gd
@@ -4,17 +4,23 @@ var config = ConfigFile.new()
 const SETTING_FILE_PATH = "user://settings.ini"
 
 func _ready() -> void:
+	#se non esiste un settings.ini nel path allora ne creo uno con gli elementi da salvare
+	#[Audio]
+	#-master_volume = 1.0
+	#-sfx = 1.0
 	if !FileAccess.file_exists(SETTING_FILE_PATH):
 		config.set_value("audio", "master_volume", 1.0)
 		config.save(SETTING_FILE_PATH)
 	else:
+	#altrimenti carico i dati del file settings.ini
 		config.load(SETTING_FILE_PATH)
 		
-
+	#funzione che permette di salvare nel "ramo" audio i valori di una chiave es. master_volume e il valore
 func save_audio_setting(key: String, value):
 	config.set_value("audio", key, value)
 	config.save(SETTING_FILE_PATH)
-
+	#funzione per caricare i settings audio, per ogni chiave (es. master_volume, sfx etc... nella sezione "audio"
+	#salvo nella variabile audio_settings la chiave.
 func load_audio_setting():
 	var audio_settings = {}
 	for key in config.get_section_keys("audio"):
--- menus/scripts/option_menu.gd
+++ menus/scripts/option_menu.gd
@@ -3,7 +3,9 @@ extends Control
 @onready var master_slide: HSlider = $AudioControl/Buttons_cont/Master_slide
 func _ready() -> void:
 	print(db_to_linear(AudioServer.get_bus_volume_db(0)))
+	#vado a caricare i salvataggi settings
 	var audio_settings = ConfigFileHandler.load_audio_setting()
+	#vado a settare la barra del master in base al valore salvato nel config 
 	master_slide.value = min(audio_settings.master_volume, 1.0)*100
 	change_value(master_slide.value, 0)
 	#master_slide.value = db_to_linear(AudioServer.get_bus_volume_db(0))
@@ -18,6 +20,7 @@ func _on_button_back_pressed() -> void:
 
 func _on_master_slide_drag_ended(value_changed: bool) -> void:
 	if value_changed:
+		#se il valore è cambiato allora vado a chiamare la funzione per passare i dati da salvare
 		ConfigFileHandler.save_audio_setting("master_volume", master_slide.value/100)
 		change_value(master_slide.value, 0)
 
@@ -28,7 +31,7 @@ func _on_music_slide_drag_ended(value_changed: bool) -> void:
 func _on_sfx_slide_drag_ended(value_changed: bool) -> void:
 	pass # Replace with function body.
 	
-	
+	#funzione per cambiare il volume bus in audio in base a dove chiami
 func change_value(value: float, index: int):
 	AudioServer.set_bus_volume_db(index, linear_to_db(value))
 	print("funzione", db_to_linear(AudioServer.get_bus_volume_db(0)))
