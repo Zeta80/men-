63ab526fb6079582cdf63ee855cb74e540e4b966
Zeta80 <axxel1700@gmail.com>
2025-02-09 22:33:45 +0100

    fixed option finish menu alpha 3.1

--- Example_level/game_controller.gd
+++ Example_level/game_controller.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 @export var pause_menu: PackedScene
-var pause_menu_instance 
+@export var option_menu: PackedScene
+var pause_menu_instance
+var option_menu_instance 
 var pause_already_pressed := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -32,6 +34,7 @@ func _input(event: InputEvent) -> void:
 func on_pause_delete():
 	print("funzioone")
 	remove_child(pause_menu_instance)
+	hide_option_menu()
 	pause_game(false)
 	
 func pause_game(value: bool) -> void:
@@ -42,11 +45,15 @@ func restart_game() -> void:
 	get_tree().reload_current_scene()
 	
 func option_game() -> void:
-	pause_game(false)
-	get_tree().change_scene_to_file("res://menus/scenes/option_menu.tscn")
+	pause_menu_instance.visible = false
+	option_menu_instance = option_menu.instantiate()
+	add_child(option_menu_instance)
+	option_menu_instance.connect("hide_option_menu", Callable(self, "hide_option_menu"))
 	
 func main_menu_game() -> void:
 	pause_game(false)
 	get_tree().change_scene_to_file("res://menus/scenes/main_menu.tscn")
 	
-	
+func hide_option_menu() -> void:
+	remove_child(option_menu_instance)
+	pause_menu_instance.visible = true
